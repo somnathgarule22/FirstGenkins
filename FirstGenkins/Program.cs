a4e3501212e73cffc3d5aa564578fe984a7d2a05
Somnathgarule22 <somnathgarule22@gmail.com>
2025-07-25 08:34:35 +0530

    Add project files.

---
+++ FirstGenkins/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstGenkins
+{
+    internal class Program
+    {
+        public void Addition()
+        {
+            int a = 50, b = 40, c;
+            c = a + b;
+            Console.WriteLine("Addition = " + c);
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
